d8e3ceeda63711ce4a7755096c80541e931f0f1c
brtkglnski <glinskib2008@gmail.com>
2025-01-22 21:27:42 +0100

    Experience component

---
+++ src/components/Experience.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { useState } from "react";
+const Experience = () => {
+    const [activeTab, setActiveTab] = useState("work");
+    return (
+        <div className="w-full h-4/5 lg:h-3/5 flex flex-col justify-center p-4">
+            <div className="w-full border-2 border-white h-12 w-8 rounded-md flex flex-row items-center">
+            <button className={`w-1/2 rounded-md h-full flex items-center justify-center
+                ${activeTab === "work"
+                      ? "bg-white"
+                      : "text-white"
+                  }`}
+                onClick={()=> setActiveTab("work")}>Work</button>
+            <button 
+            className={`w-1/2  rounded-md h-full flex items-center justify-center
+            ${activeTab === "education"
+                      ? "bg-white"
+                      : "text-white"
+                  }`}
+                  onClick={()=> setActiveTab("education")}>Education</button>
+            </div>
+            <div className="border-2 w-full h-full rounded-md mt-4 lg:mt-2 text-white flex justify-center items-center">
+                {activeTab === "work" ? "experience here" : "education here"}
+            </div>
+        </div>
+    )
+}
+export default Experience;
