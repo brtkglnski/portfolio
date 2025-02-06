65297fd81d37a7f17eb59315abe01f2bd05823a8
brtkglnski <glinskib2008@gmail.com>
2025-02-06 19:08:56 +0100

    Details for deployment

--- vite.config.cjs
+++ vite.config.cjs
@@ -5,7 +5,7 @@ import tailwindcss from 'tailwindcss'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio",
+  base: "/portfolio/",
   css: {
     postcss: {
       plugins: [tailwindcss()],
